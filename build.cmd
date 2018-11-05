333da6cddfad2dacf748ccb12bd77e17361fe65d
Richie Zhang <richiezhang256@gmail.com>
2018-11-04 18:11:14 -0800

    Create "startup.py".
    This file will be used for the Podcast Downloader GUI.

--- build.cmd
+++ build.cmd
@@ -1,5 +1,12 @@
 @echo off
 
+pyinstaller startup.py ^
+--onefile ^
+--log-level=DEBUG ^
+--specpath=.\build\ ^
+--name="PodcastDownloader" ^
+--icon=.\icon\icon.ico
+
 pyinstaller startup_cli.py ^
 --onefile ^
 --log-level=DEBUG ^
