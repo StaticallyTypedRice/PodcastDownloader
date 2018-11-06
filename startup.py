b7bff40283d19fb12d73a1240008b5c82456065b
Richie Zhang <richiezhang256@gmail.com>
2018-11-06 00:06:08 -0800

    Create a blank startup function in startup.py

---
+++ startup.py
@@ -0,0 +1,11 @@
+from defusedxml import ElementTree
+
+from modules.xml import get_unique_xml_element, parse_remote_xml
+from modules.download import podcast_download
+
+def startup():
+    '''The startup GUI function.'''
+    pass
+
+if __name__ == '__main__':
+    startup()
