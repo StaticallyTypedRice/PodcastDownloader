1fa488deaee60d55fb09153483c75ff7280617ec
Richie Zhang <richiezhang256@gmail.com>
2018-11-08 01:05:48 -0800

    Move source files to the "src" folder

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-pyinstaller startup.py \
+pyinstaller ./src/startup.py \
 --onefile \
 --log-level=DEBUG \
 --specpath=./build/ \
 --name="PodcastDownloader" \
---icon=./icon/icon.ico
+--icon=./src/icon/icon.ico
 
-pyinstaller startup_cli.py \
+pyinstaller ./src/startup_cli.py \
 --onefile \
 --log-level=DEBUG \
 --specpath=./build/ \
 --name="PodcastDownloaderCLI" \
---icon=./icon/icon.ico
+--icon=./src/icon/icon.ico
