006d62a2eba32bcefdb024de4df83cda36dcf916
Richie Zhang <richiezhang256@gmail.com>
2018-11-09 19:05:43 -0800

    Create a blank GUI with TkInter.

--- src/startup.py
+++ src/startup.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 from defusedxml import ElementTree
 
 from modules.xml import get_unique_xml_element, parse_remote_xml
@@ -5,7 +7,14 @@ from modules.download import podcast_download
 
 def startup():
     '''The startup GUI function.'''
-    pass
+    
+    # Create the TkInter window
+    window = tk.Tk()
+
+    window.title("Podcast Downloader")
+
+    # Start the GUI
+    window.mainloop()
 
 if __name__ == '__main__':
     startup()
